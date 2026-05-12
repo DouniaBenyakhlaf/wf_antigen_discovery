fcc4167395339b2e04392db11689a4739b2bea22
Mariia Koshkina <mariia.koshkina@curie.fr>
2026-05-12 17:16:46 +0200

    Added an example

--- workflow/profiles/mariia/todo.py
+++ workflow/profiles/mariia/todo.py
@@ -3,8 +3,13 @@ SEARCHES.append(AntigeneSearch(project="example",
                               input_fasta="path/to/your/neotranscript/fasta", 
                               hla_txt="path/to/your/hla/alleles.txt", 
                               immunopeptidomics_data="path/to/your/immunopeptidomics/data"))
-'''
+
 SEARCHES.append(AntigeneSearch(project="EPS_DDA_MHCI_GRU1",
                                 input_fasta="/mnt/raid0/EPS/EPS_extra_genes.index_specific.groupby_index.fasta",
                                 hla_txt="/mnt/raid0/EPS/data/HLAs/GRU_HLAs.txt",
                                 immunopeptidomics_data="/mnt/raid0/EPS/data/Immunopeptidomics/DDA_MHC1_GRU1"))
+'''
+SEARCHES.append(AntigeneSearch(project="example",
+                                input_fasta="example/example_MLANA.fasta",
+                                hla_txt="example/example_OD5P_hlas.txt",
+                                immunopeptidomics_data="_data/example"))
